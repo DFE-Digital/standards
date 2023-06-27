faff3652c8a0731c6aaceb988efa3552c4708635
Andy Jones <andy.jones@digital.education.gov.uk>
2023-06-27 21:12:44 +0100

    Initial version

---
+++ index.js
@@ -0,0 +1,174 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const axios = require('axios')
+var dateFilter = require('nunjucks-date-filter')
+var markdown = require('nunjucks-markdown')
+var marked = require('marked')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const path = require('path')
+const config = require('./app/config')
+const forceHttps = require('express-force-https');
+const compression = require('compression');
+
+const favicon = require('serve-favicon');
+
+const PageIndex = require('./middleware/pageIndex')
+const pageIndex = new PageIndex(config)
+require('dotenv').config()
+var NotifyClient = require('notifications-node-client').NotifyClient
+
+const app = express()
+app.use(compression());
+
+const notify = new NotifyClient(process.env.notifyKey)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(favicon(path.join(__dirname, 'public/assets/images', 'favicon.ico')));
+
+app.set('view engine', 'html')
+
+app.locals.serviceName = config.serviceName
+app.locals.recaptchaPublic = process.env.recaptchaPublic
+
+// Set up Nunjucks as the template engine
+var nunjuckEnv = nunjucks.configure(
+  [
+    'app/views',
+    'node_modules/govuk-frontend',
+    'node_modules/dfe-frontend-alpha/packages/components',
+  ],
+  {
+    autoescape: true,
+    express: app,
+  },
+)
+
+nunjuckEnv.addFilter('date', dateFilter)
+markdown.register(nunjuckEnv, marked.parse)
+
+nunjuckEnv.addFilter('formatNumber', function (number) {
+  return number.toLocaleString();
+});
+
+app.use(forceHttps);
+
+// Set up static file serving for the app's assets
+app.use('/assets', express.static('public/assets'))
+
+app.use((req, res, next) => {
+  if (req.url.endsWith('/') && req.url.length > 1) {
+    const canonicalUrl = req.url.slice(0, -1);
+    res.set('Link', `<${canonicalUrl}>; rel="canonical"`);
+  }
+  next();
+});
+
+
+app.get('/robots.txt', (_, res) => {
+  res.set({ 'Content-Type': 'text/plain' });
+  res.render('robots.txt');
+});
+
+
+
+app.post('/submit-feedback', (req, res) => {
+  const feedback = req.body.feedback_form_input
+  const fullUrl = req.headers.referer || 'Unknown'
+
+  //Send to notify after validation with recaptcha first
+  //TODO: Implement recaptcha
+
+  notify
+    .sendEmail(process.env.feedbackTemplateID, 'design.ops@education.gov.uk', {
+      personalisation: {
+        feedback: feedback,
+        page: fullUrl,
+        service: "Design Manual"
+      },
+    })
+    .then((response) => { })
+    .catch((err) => console.log(err))
+
+  return res.sendStatus(200)
+})
+
+
+
+app.get(/\.html?$/i, function (req, res) {
+  var path = req.path
+  var parts = path.split('.')
+  parts.pop()
+  path = parts.join('.')
+  res.redirect(path)
+})
+
+app.get(/^([^.]+)$/, function (req, res, next) {
+  matchRoutes(req, res, next)
+})
+
+// Handle 404 errors
+app.use(function (req, res, next) {
+  res.status(404).render('error.html')
+})
+
+// Handle 500 errors
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).render('error.html')
+})
+
+// Try to match a request to a template, for example a request for /test
+// would look for /app/views/test.html
+// and /app/views/test/index.html
+
+function renderPath(path, res, next) {
+  // Try to render the path
+  res.render(path, function (error, html) {
+    if (!error) {
+      // Success - send the response
+      res.set({ 'Content-type': 'text/html; charset=utf-8' })
+      res.end(html)
+      return
+    }
+    if (!error.message.startsWith('template not found')) {
+      // We got an error other than template not found - call next with the error
+      next(error)
+      return
+    }
+    if (!path.endsWith('/index')) {
+      // Maybe it's a folder - try to render [path]/index.html
+      renderPath(path + '/index', res, next)
+      return
+    }
+    // We got template not found both times - call next to trigger the 404 page
+    next()
+  })
+}
+
+matchRoutes = function (req, res, next) {
+  var path = req.path
+
+  // Remove the first slash, render won't work with it
+  path = path.substr(1)
+
+  // If it's blank, render the root index
+  if (path === '') {
+    path = 'index'
+  }
+
+  renderPath(path, res, next)
+}
+
+// Start the server
+
+// Run application on configured port
+if (config.env === 'development') {
+  app.listen(config.port - 50, () => {
+  });
+} else {
+  app.listen(config.port);
+}
+
+app.listen(config.port)
